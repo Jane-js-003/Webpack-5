de1fb104b09f57e23fcb3d41ca2c1a81ea0b5f7e
account dingmengjuan <account dingmengjuan@360.cn>
2021-04-15 20:05:59 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  mode: 'development',
+  mode: 'development', // 开发（development）/生产（production）环境
   entry: './src/index.js',
   output: {
     // __dirname 当前的文件所在的目录
     path: resolve(__dirname, 'dist'), // 输出文件夹的绝对路径
     filename: 'main.js' // 输出的文件名
-  }
+  },
+  module: {
+    rules: [
+      {test:/\.txt$/, use:'raw-loader'}
+    ]
+  },
+  plugins: [ // 插件
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
