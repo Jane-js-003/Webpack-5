87b19e6f89bcfb527923dd6d24b095bb7cbb1d8c
account dingmengjuan <account dingmengjuan@360.cn>
2021-04-15 20:41:24 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,16 @@ module.exports = {
     path: resolve(__dirname, 'dist'), // 输出文件夹的绝对路径
     filename: 'main.js' // 输出的文件名
   },
+  // devServer会启动一个HTTP开发服务器，会把一个文件夹作为静态根目录
+  // 为了提高性能，使用的是内存文件系统
+  // devServer默认情况下会读取打包后的路径，其实静态文件的根目录是可以有多个的
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    compress: true, // 是否启动压缩
+    writeToDisk: true, // 如果你指定此选项，也会把打包后的文件写入硬盘一份（即生成打包后的dist文件夹,写入硬盘为了方便看，方便调整）
+    port: 8080, // 指定服务器的端口
+    open: true // 自动打开浏览器
+  },
   module: {
     rules: [
       {test:/\.txt$/, use:'raw-loader'}
