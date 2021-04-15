e701f7d6d875ab28a53546675a613f8dd539f53b
account dingmengjuan <account dingmengjuan@360.cn>
2021-04-15 16:46:58 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const {resolve} = require('path')
 module.exports = {
-  entry: './src/index.js'
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    // __dirname 当前的文件所在的目录
+    path: resolve(__dirname, 'dist'), // 输出文件夹的绝对路径
+    filename: 'main.js' // 输出的文件名
+  }
 }
